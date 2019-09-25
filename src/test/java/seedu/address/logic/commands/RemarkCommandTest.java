e0d740f39fa75765ed1913cadf3fc465799ff1cc
Jonathan Tjendana <jonathan.tjendana@gmail.com>
2019-09-25 17:59:14 +0800

    Code Quality: removed unused fields and avoided using equals to compare against null

--- src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -116,7 +116,7 @@ public class RemarkCommandTest {
         assertTrue(standardCommand.equals(standardCommand));
 
         // null -> returns false
-        assertFalse(standardCommand.equals(null));
+        assertFalse(standardCommand == null);
 
         // different types -> returns false
         assertFalse(standardCommand.equals(new ClearCommand()));
